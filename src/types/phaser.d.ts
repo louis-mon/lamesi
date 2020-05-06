c4ba12b01a1286122692044e4a1eb99f348a8ba3
Louis Monestier <louis.monestier@hotmail.fr>
2020-05-07 01:23:41 +0200

    fix type

--- src/types/phaser.d.ts
+++ src/types/phaser.d.ts
@@ -1,27 +1,26 @@
-import Pointer = Phaser.Input.Pointer;
-
-type EventGameObjectMapping = {
-  drag: (pointer: Pointer, dragX: number, dragY: number) => void;
-  pointerdown: (
-    pointer: Pointer,
-    localX: number,
-    localY: number,
-    event: Phaser.Types.Input.EventData
-  ) => void;
-};
-
-declare namespace Phaser {
-  namespace GameObjects {
-    interface GameObject {
-      on<E extends keyof EventGameObjectMapping, T extends GameObject>(
-        this: T,
-        e: E,
-        f: EventGameObjectMapping[E]
-      ): T;
-    }
-  }
-}
+import * as P from "phaser";
+import Pointer = P.Input.Pointer;
 
 declare module "phaser" {
+  namespace Phaser {
+    type EventGameObjectMapping = {
+      drag: (pointer: Pointer, dragX: number, dragY: number) => void;
+      pointerdown: (
+        pointer: Pointer,
+        localX: number,
+        localY: number,
+        event: P.Types.Input.EventData
+      ) => void;
+    };
+    namespace GameObjects {
+      export interface GameObject {
+        on<E extends keyof EventGameObjectMapping, T extends GameObject>(
+          this: T,
+          e: E,
+          f: EventGameObjectMapping[E]
+        ): T;
+      }
+    }
+  }
   export = Phaser;
 }
