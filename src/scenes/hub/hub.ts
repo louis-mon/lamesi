81a6a5be1609c066d9af246e8852418b75171dab
louis <louis.monestier@hotmail.fr>
2020-05-18 00:52:29 +0200

    fix hub issue

--- src/scenes/hub/hub.ts
+++ src/scenes/hub/hub.ts
@@ -66,6 +66,7 @@ export class HubScene extends Phaser.Scene {
                 currentScene: scene,
                 parentScene: this,
               });
+              this.scene.setActive(false);
             },
           });
           scenes.forEach((otherScene) => {
