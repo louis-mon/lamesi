1861e1a2fb317963c1a3372b48c9727420e8767d
louis <louis.monestier@hotmail.fr>
2021-03-02 09:18:24 +0100

    wip tree

---
+++ src/scenes/creatures/def.ts
@@ -0,0 +1,4 @@
+export const dephts = {
+  treeTrunk: 10,
+  treeBud: 20,
+};
--- src/scenes/creatures/tree.ts
+++ src/scenes/creatures/tree.ts
@@ -19,6 +19,7 @@ import {
 import { combineContext } from "/src/helpers/functional";
 import { combineLatest, fromEvent } from "rxjs";
 import { map } from "rxjs/operators";
+import * as Def from "./def";
 
 type CreateBudParams = {
   pos: Vector2;
@@ -28,28 +29,63 @@ type CreateBudParams = {
 
 const createBud = (params: CreateBudParams): Flow.PhaserNode =>
   Flow.lazy((scene) => {
+    const displayRot = (angle: number) => angle + Math.PI / 2;
     const bud = createSpriteAt(scene, params.pos, "tree", "bud")
       .setScale(0)
-      .setRotation(params.angleR);
+      .setRotation(displayRot(params.angleR))
+      .setDepth(Def.dephts.treeBud)
+      .setInteractive();
     const budInst = declareGoInstance(spriteClassKind, null);
     budInst.create(bud);
-    return Flow.sequence(
-      Flow.tween({ targets: bud, props: { scale: 1 } }),
-      Flow.wait(commonGoEvents.pointerdown(budInst.key).subject),
-      createBud({
-        pos: Phaser.Math.RotateTo(
-          new Vector2(),
-          params.pos.x,
-          params.pos.y,
-          params.angleR,
-          50,
+
+    const createSubBud = (dAngle: number) => {
+      const newAngle =
+        params.angleR +
+        dAngle * ((Math.PI * 2) / 3 / Math.pow(1.7, params.level));
+      const length = 150 / Math.pow(1.5, params.level);
+      const targetPos = Phaser.Math.RotateTo(
+        new Vector2(),
+        params.pos.x,
+        params.pos.y,
+        newAngle,
+        length,
+      );
+      const trunk = createSpriteAt(scene, params.pos, "tree", "trunk")
+        .setDepth(Def.dephts.treeTrunk)
+        .setRotation(displayRot(newAngle))
+        .setOrigin(0.5, 1);
+      trunk.setScale(length / trunk.height, 0);
+      return Flow.sequence(
+        Flow.parallel(
+          Flow.sequence(
+            Flow.tween({ targets: bud, props: { scale: 0 }, duration: 400 }),
+            Flow.call(() => bud.destroy()),
+          ),
+          Flow.tween({
+            targets: trunk,
+            props: { scaleY: length / trunk.height },
+            duration: 600,
+          }),
         ),
-        level: params.level + 1,
-        angleR: params.angleR,
-      }),
+        createBud({
+          pos: targetPos,
+          level: params.level + 1,
+          angleR: newAngle,
+        }),
+      );
+    };
+
+    return Flow.sequence(
+      Flow.tween({ targets: bud, props: { scale: 1 }, duration: 500 }),
+      Flow.wait(commonGoEvents.pointerdown(budInst.key).subject),
+      Flow.lazy(() =>
+        params.level === 0
+          ? createSubBud(0)
+          : Flow.parallel(createSubBud(1), createSubBud(-1)),
+      ),
     );
   });
 
 export const createTree: Flow.PhaserNode = Flow.lazy((scene) =>
-  createBud({ pos: new Vector2(1400, 700), level: 0, angleR: 0 }),
+  createBud({ pos: new Vector2(1400, 700), level: 0, angleR: -Math.PI / 2 }),
 );
